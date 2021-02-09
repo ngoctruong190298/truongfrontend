bd9b5436714c30abff44b491b112c5718e15ca01
= <=>
2021-02-09 06:33:07 -0500

    commit to reponsitory

---
+++ assets/js/modal.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById("menu");
+var btn = document.getElementById("icon");
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
